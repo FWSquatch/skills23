e43cce716175e8993447201c8e2514b9f96ef731
FWSquatch <joshosu@gmail.com>
2023-02-12 11:59:45 -0600

    decoder

--- challenges/encoding/pix.py
+++ challenges/encoding/pix.py
@@ -157,5 +157,13 @@ convert(message)
 
 pix_off()
 
-#    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
-#
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+
